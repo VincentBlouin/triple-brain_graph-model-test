5b29bda072002f74057f2d78944be0f4342b723f
Vincent Blouin <vincent.blouin@gmail.com>
2015-10-24 15:17:21 -0400

    debug number of references

--- src/test/java/guru/bubl/test/module/model/ModelSpecificClassTest.java
+++ src/test/java/guru/bubl/test/module/model/ModelSpecificClassTest.java
@@ -13,9 +13,8 @@ import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-@Ignore
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-       WholeGraphAdminTest.class
+       WholeGraphTest.class
 })
 public class ModelSpecificClassTest extends ModelTestRunner {}
