2d893884f4577c02d904fe6bcc7c5d4bfb7fa6b3
Vincent Blouin <vincent.blouin@gmail.com>
2018-01-07 21:08:39 -0500

    tag merge to

--- src/test/java/guru/bubl/test/module/model/ModelSpecificClassTest.java
+++ src/test/java/guru/bubl/test/module/model/ModelSpecificClassTest.java
@@ -6,6 +6,7 @@ package guru.bubl.test.module.model;
 
 import guru.bubl.module.model.graph.GraphElementOperator;
 import guru.bubl.test.module.model.graph.*;
+import guru.bubl.test.module.model.graph.meta.IdentificationOperatorTest;
 import guru.bubl.test.module.model.graph.search.GraphIndexerTest;
 import guru.bubl.test.module.utils.ModelTestRunner;
 import org.junit.Ignore;
@@ -14,7 +15,7 @@ import org.junit.runners.Suite;
 @Ignore
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        VertexOperatorTest.class
+        IdentificationOperatorTest.class
 
 })
 public class ModelSpecificClassTest extends ModelTestRunner {}
--- src/test/java/guru/bubl/test/module/model/graph/meta/IdentificationOperatorTest.java
+++ src/test/java/guru/bubl/test/module/model/graph/meta/IdentificationOperatorTest.java
@@ -5,19 +5,23 @@
 package guru.bubl.test.module.model.graph.meta;
 
 import guru.bubl.module.model.graph.identification.IdentificationOperator;
+import guru.bubl.module.model.graph.identification.Identifier;
 import guru.bubl.module.model.graph.identification.IdentifierPojo;
 import guru.bubl.test.module.utils.ModelTestResources;
+import guru.bubl.test.module.utils.ModelTestScenarios;
 import org.junit.Test;
 
 import java.net.URI;
 
+import static junit.framework.TestCase.assertTrue;
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
 
 public class IdentificationOperatorTest extends ModelTestResources {
 
     @Test
-    public void can_set_number_of_references(){
+    public void can_set_number_of_references() {
         IdentifierPojo identificationPojo = vertexA.addMeta(
                 modelTestScenarios.tShirt()
         ).values().iterator().next();
@@ -36,7 +40,7 @@ public class IdentificationOperatorTest extends ModelTestResources {
     }
 
     @Test
-    public void can_build_pojo(){
+    public void can_build_pojo() {
         IdentifierPojo identificationPojo = vertexA.addMeta(
                 modelTestScenarios.location()
         ).values().iterator().next();
@@ -64,6 +68,94 @@ public class IdentificationOperatorTest extends ModelTestResources {
         assertThat(
                 identificationPojo.comment(),
                 is("The Location type is used for any topic with a fixed location on the planet Earth. It includes geographic features such as oceans and mountains, political entities like cities and man-made objects like buildings.Guidelines for filling in location properties:geolocation: the longitude and latitude (in decimal notation) of the feature, or of the geographical center (centroid) fo the feature.contains and contained by: these properties can be used to show spatial relationships between different locations, such as an island contained by a body of water (which is equivalent to saying the body of water contains the island), a state contained by a country, a mountain within the borders of a national park, etc. For geopolitical locations,   containment two levels up and down is the ideal minimum. For example, the next two levels up for the city of Detroit are Wayne County and the state of Michigan.adjoins: also used to show spatial relations, in this case between locations that share a border.USBG Name: A unique name given to geographic features within the U.S. and its territories by the United States Board on Geographic Names. More information can be found on their website. GNIS ID: A unique id given to geographic features within the U.S. and its territories by the United States Board on Geographic Names. GNIS stands for Geographic Names Information System. More information can be found on their website.GEOnet Feature ID: The UFI (Unique Feature ID) used by GeoNet for features outside of the United States. More information can be found on their website.")
+        );
+    }
+
+    @Test
+    public void mergeTo_removes_tag() {
+        IdentifierPojo personFromFreebase = vertexA.addMeta(
+                modelTestScenarios.personFromFreebase()
+        ).values().iterator().next();
+
+        IdentifierPojo personTag = vertexA.addMeta(
+                modelTestScenarios.person()
+        ).values().iterator().next();
+        assertTrue(
+                userGraph.haveElementWithId(
+                        personFromFreebase.uri()
+                )
+        );
+        identificationFactory.withUri(
+                personFromFreebase.uri()
+        ).mergeTo(personTag);
+        assertFalse(
+                userGraph.haveElementWithId(
+                        personFromFreebase.uri()
+                )
+        );
+    }
+
+    @Test
+    public void mergeTo_includes_tagged_graph_elements() {
+        IdentifierPojo personFromFreebase = vertexA.addMeta(
+                modelTestScenarios.personFromFreebase()
+        ).values().iterator().next();
+        IdentifierPojo personTag = vertexB.addMeta(
+                modelTestScenarios.person()
+        ).values().iterator().next();
+        vertexC.addMeta(
+                personFromFreebase
+        );
+        assertThat(
+                identificationFactory.withUri(
+                        personTag.uri()
+                ).getNbReferences(),
+                is(1)
+        );
+        assertFalse(
+                vertexA.getIdentifications().containsKey(
+                        personTag.getExternalResourceUri()
+                )
+        );
+        identificationFactory.withUri(
+                personFromFreebase.uri()
+        ).mergeTo(personTag);
+        assertThat(
+                identificationFactory.withUri(
+                        personTag.uri()
+                ).getNbReferences(),
+                is(3)
+        );
+        assertTrue(
+                vertexA.getIdentifications().containsKey(
+                        personTag.getExternalResourceUri()
+                )
+        );
+    }
+
+    @Test
+    public void mergeTo_excludes_non_related_graph_elements() {
+        vertexC.addMeta(modelTestScenarios.computerScientistType());
+        vertexB.addVertexAndRelation();
+        IdentifierPojo personFromFreebase = vertexA.addMeta(
+                modelTestScenarios.personFromFreebase()
+        ).values().iterator().next();
+        IdentifierPojo person = vertexB.addMeta(
+                modelTestScenarios.person()
+        ).values().iterator().next();
+        identificationFactory.withUri(
+                personFromFreebase.uri()
+        ).mergeTo(person);
+        assertThat(
+                identificationFactory.withUri(
+                        person.uri()
+                ).getNbReferences(),
+                is(2)
+        );
+        assertFalse(
+                vertexC.getIdentifications().containsKey(
+                        person.getExternalResourceUri()
+                )
         );
     }
 }
