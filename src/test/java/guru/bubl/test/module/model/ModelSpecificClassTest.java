de123dfeea44c6e3597015e8fda1257df4632a7c
Vincent Blouin <vincent.blouin@gmail.com>
2019-09-24 16:17:34 -0400

    remove props source_destination_vertex_uri to edges and removed static content

--- src/test/java/guru/bubl/test/module/model/ModelSpecificClassTest.java
+++ src/test/java/guru/bubl/test/module/model/ModelSpecificClassTest.java
@@ -4,27 +4,7 @@
 
 package guru.bubl.test.module.model;
 
-import guru.bubl.module.model.FriendlyResource;
-import guru.bubl.module.model.admin.WholeGraphAdminDailyJob;
-import guru.bubl.module.model.content.AllContent;
-import guru.bubl.module.model.friend.friend_request_email.FriendRequestEmail;
-import guru.bubl.module.model.graph.edge.EdgeOperator;
-import guru.bubl.module.model.search.GraphIndexer;
-import guru.bubl.module.neo4j_graph_manipulator.graph.center_graph_element.CenterGraphElementsOperatorNeo4j;
-import guru.bubl.module.neo4j_user_repository.UserRepositoryNeo4j;
-import guru.bubl.test.module.model.center_graph_element.CenterGraphElementOperatorTest;
-import guru.bubl.test.module.model.center_graph_element.CenterGraphElementsOperatorTest;
-import guru.bubl.test.module.model.center_graph_element.WholeGraphAdminTest;
-import guru.bubl.test.module.model.graph.*;
-import guru.bubl.test.module.model.graph.meta.IdentificationOperatorTest;
-import guru.bubl.test.module.model.graph.meta.UserMetasOperatorTest;
-import guru.bubl.test.module.model.graph.pattern.PatternListTest;
-import guru.bubl.test.module.model.graph.pattern.PatternUserTest;
-import guru.bubl.test.module.model.graph.search.GraphIndexerTest;
-import guru.bubl.test.module.model.graph.search.GraphSearchTest;
-import guru.bubl.test.module.model.graph.subgraph.SubGraphForkerTest;
-import guru.bubl.test.module.model.user.FriendManagerTest;
-import guru.bubl.test.module.model.user.UserRepositoryTest;
+import guru.bubl.test.module.model.graph.UserGraphTest;
 import guru.bubl.test.module.utils.ModelTestRunner;
 import org.junit.Ignore;
 import org.junit.runner.RunWith;
