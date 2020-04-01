97208ba3221150c5e002d2fa01f3c39f9d01aab7
Vincent Blouin <vincent.blouin@gmail.com>
2020-04-01 08:07:58 -0400

    centers missing tags nb neighbors

--- src/test/java/guru/bubl/test/module/model/ModelSpecificClassTest.java
+++ src/test/java/guru/bubl/test/module/model/ModelSpecificClassTest.java
@@ -4,6 +4,7 @@
 
 package guru.bubl.test.module.model;
 
+import guru.bubl.module.neo4j_graph_manipulator.graph.center_graph_element.CenterGraphElementsOperatorNeo4j;
 import guru.bubl.test.module.model.center_graph_element.CenterGraphElementsOperatorTest;
 import guru.bubl.test.module.model.center_graph_element.WholeGraphAdminTest;
 import guru.bubl.test.module.model.graph.GraphElementOperatorTest;
@@ -16,6 +17,6 @@ import org.junit.runners.Suite;
 @Ignore
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        WholeGraphAdminTest.class
+        CenterGraphElementsOperatorTest.class
 })
 public class ModelSpecificClassTest extends ModelTestRunner {}
