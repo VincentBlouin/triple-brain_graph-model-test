5dc8c02a392b7691b1b46a19a8e4ec08a277b376
Vincent Blouin <vincent.blouin@gmail.com>
2020-04-06 10:20:49 -0400

    relationship rename

--- src/test/java/guru/bubl/test/module/model/ModelSpecificClassTest.java
+++ src/test/java/guru/bubl/test/module/model/ModelSpecificClassTest.java
@@ -7,6 +7,7 @@ package guru.bubl.test.module.model;
 import guru.bubl.module.neo4j_graph_manipulator.graph.center_graph_element.CenterGraphElementsOperatorNeo4j;
 import guru.bubl.test.module.model.center_graph_element.CenterGraphElementsOperatorTest;
 import guru.bubl.test.module.model.center_graph_element.WholeGraphAdminTest;
+import guru.bubl.test.module.model.graph.EdgeOperatorTest;
 import guru.bubl.test.module.model.graph.GraphElementOperatorTest;
 import guru.bubl.test.module.model.graph.UserGraphTest;
 import guru.bubl.test.module.model.graph.VertexOperatorTest;
@@ -17,6 +18,6 @@ import org.junit.runners.Suite;
 @Ignore
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        CenterGraphElementsOperatorTest.class
+        EdgeOperatorTest.class
 })
 public class ModelSpecificClassTest extends ModelTestRunner {}
